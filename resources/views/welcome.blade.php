f91da19055f80eadd1931019b12fc28c946e2c53
Subhan Hakim <subhanhakim07@gmail.com>
2024-05-25 22:11:18 +0700

    finish tugas SI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,19 @@
 <x-guest-layout>
     <!-- Main Hero Content -->
-    <div class="container max-w-lg px-4 py-32 mx-auto text-left bg-center bg-no-repeat bg-cover md:max-w-none md:text-center"
-        style="background-image: url('https://cdn.pixabay.com/photo/2016/11/18/14/39/beans-1834984_960_720.jpg')">
-        <h1
-            class="font-mono text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 md:text-center sm:leading-none lg:text-5xl">
-            <span class="inline md:block">Welcome To Larainfo Restaurant</span>
+    <div class="container max-w-lg px-4 py-32 mx-auto text-left bg-center  bg-no-repeat bg-cover md:max-w-none md:text-center"
+        style="background-image: url('http://127.0.0.1:8000/storage/casa.jpg')">
+        <h1 class="font-mono text-3xl font-extrabold bg-clip-text text-black md:text-center sm:leading-none lg:text-5xl">
+            <span class="inline md:block">Selamat Datang di <span class="text-green-600">Die Restaurant</span></span>
         </h1>
-        <div class="mx-auto mt-2 text-green-50 md:text-center lg:text-lg">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta voluptatem ab necessitatibus illo
-            praesentium
-            culpa excepturi quae commodi quaerat,
+        <div class=" mx-auto mt-2 text-green-50 md:text-center lg:text-lg w-1/2">
+            Nikmati kelezatan hidangan khas kami yang dibuat dengan bahan-bahan segar dan berkualitas. Temukan menu
+            lengkap kami, reservasi meja, dan promo menarik hanya di sini.
         </div>
         <div class="flex flex-col items-center mt-12 text-center">
             <span class="relative inline-flex w-full md:w-auto">
                 <a href="{{ route('reservations.step.one') }}" type="button"
                     class="inline-flex items-center justify-center px-6 py-2 text-base font-bold leading-6 text-white bg-green-600 rounded-full lg:w-full md:w-auto hover:bg-green-500 focus:outline-none">
-                    Make Your Reservation
+                    Buat Reservasi
                 </a>
         </div>
     </div>
@@ -28,16 +26,15 @@
                         <!-- <h1
         class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl"
       > -->
-                        <h3 class="text-xl">OUR STORY
+                        <h3 class="text-xl">Sejarah
                         </h3>
-                        <h2 class="text-4xl text-green-600">Welcome</h2>
+                        <h2 class="text-4xl text-green-600">Selamat datang di Dei Restaurant</h2>
                         <!-- </h1> -->
                         <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nemo incidunt
-                            praesentium, ipsum
-                            culpa minus eveniet, id nesciunt excepturi sit voluptate repudiandae. Explicabo, incidunt
-                            quia.
-                            Repellendus mollitia quaerat est voluptas!
+                            tempat di mana cita rasa bertemu dengan tradisi. Restoran kami berdiri sejak tahun 1999,
+                            berawal dari sebuah impian untuk menyajikan hidangan lezat dengan kualitas terbaik. Berawal
+                            dari resep-resep keluarga yang turun-temurun, kami menggabungkan teknik memasak tradisional
+                            dengan sentuhan modern.
                         </p>
                         <div class="relative flex">
                             <a href="#_"
@@ -63,19 +60,18 @@
     </section>
     <section class="py-20 bg-gray-50">
         <div class="container items-center max-w-6xl px-4 mx-auto sm:px-20 md:px-32 lg:px-16">
-            <div class="flex flex-wrap items-center -mx-3">
+            <div class="flex flex-wrap items-center gap-10 -mx-3">
                 <div class="order-1 w-full px-3 lg:w-1/2 lg:order-0">
                     <div class="w-full lg:max-w-md">
-                        <h2 class="mb-4 text-2xl font-bold">About Us</h2>
-                        <h2
-                            class="mb-4 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
-                            WHY CHOOSE US?</h2>
+                        <h2 class="mb-4 text-2xl font-bold">Tentang</h2>
+                        <h2 class="mb-4 text-3xl font-bold text-green-600">
+                            Kenapa harus kami ?</h2>
 
-                        <p class="mb-4 font-medium tracking-tight text-gray-400 xl:mb-6">Lorem ipsum dolor sit amet
-                            consectetur
-                            adipisicing elit. Natus hic atque magni minus aliquam, eos quam incidunt nam iusto sunt
-                            voluptates
-                            inventore a veritatis doloremque corrupti. Veritatis est expedita cupiditate!</p>
+                        <p class="mb-4 font-medium tracking-tight text-gray-400 xl:mb-6">Dei Restaurant, kami bangga
+                            dengan komitmen kami untuk menyajikan pengalaman kuliner yang tak terlupakan. Setiap
+                            hidangan yang kami sajikan adalah hasil dari perpaduan antara keahlian memasak yang tinggi
+                            dan bahan-bahan berkualitas terbaik.
+                        </p>
                         <ul>
                             <li class="flex items-center py-2 space-x-4 xl:py-3">
                                 <svg class="w-8 h-8 text-gray-500" fill="none" stroke="currentColor"
@@ -84,7 +80,7 @@
                                         d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z">
                                     </path>
                                 </svg>
-                                <span class="font-medium text-gray-500">Faster Processing and Delivery</span>
+                                <span class="font-medium text-gray-500">Pemrosesan dan Pengiriman Lebih Cepat</span>
                             </li>
                             <li class="flex items-center py-2 space-x-4 xl:py-3">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-500" fill="none"
@@ -92,7 +88,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
-                                <span class="font-medium text-gray-500">Easy Payments</span>
+                                <span class="font-medium text-gray-500">Pembayaran Mudah</span>
                             </li>
                             <li class="flex items-center py-2 space-x-4 xl:py-3">
                                 <svg class="w-8 h-8 text-gray-500" fill="none" stroke="currentColor"
@@ -101,102 +97,44 @@
                                         d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z">
                                     </path>
                                 </svg>
-                                <span class="font-medium text-gray-500">100% Protection and Security for Your App</span>
+                                <span class="font-medium text-gray-500">
+                                    Perlindungan dan Keamanan 100% untuk Aplikasi Anda</span>
                             </li>
                         </ul>
                     </div>
                 </div>
-                <div class="w-full px-3 mb-12 lg:w-1/2 order-0 lg:order-1 lg:mb-0"><img
-                        class="mx-auto sm:max-w-sm lg:max-w-full"
-                        src="https://cdn.pixabay.com/photo/2020/12/31/12/28/cook-5876388_960_720.png"
-                        alt="feature image"></div>
+                <div class="w-full md:w-1/2">
+                    <div class="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+                        <img src="http://127.0.0.1:8000/storage/chef.jpg" />
+                    </div>
+                </div>
             </div>
         </div>
     </section>
     @if (is_array($specials))
-    <section class="mt-8 bg-white">
-        <div class="mt-4 text-center">
-            <h3 class="text-2xl font-bold">Our Menu</h3>
-            <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
-                TODAY'S SPECIALITY</h2>
-        </div>
-        <div class="container w-full px-5 py-6 mx-auto">
-            <div class="grid lg:grid-cols-4 gap-y-6">
-                @foreach ($specials->menus as $menu)
-                    <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
-                        <img class="w-full h-48" src="{{ Storage::url($menu->image) }}" alt="Image" />
-                        <div class="px-6 py-4">
-                            <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">
-                                {{ $menu->name }}</h4>
-                            <p class="leading-normal text-gray-700">{{ $menu->description }}.</p>
-                        </div>
-                        <div class="flex items-center justify-between p-4">
-                            <span class="text-xl text-green-600">${{ $menu->price }}</span>
-                        </div>
-                    </div>
-                @endforeach
+        <section class="mt-8 bg-white">
+            <div class="mt-4 text-center">
+                <h3 class="text-2xl font-bold">Our Menu</h3>
+                <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r text-green-600">
+                    TODAY'S SPECIALITY</h2>
             </div>
-        </div>
-    </section>
+            <div class="container w-full px-5 py-6 mx-auto">
+                <div class="grid lg:grid-cols-4 gap-y-6">
+                    @foreach ($specials->menus as $menu)
+                        <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+                            <img class="w-full h-48" src="{{ Storage::url($menu->image) }}" alt="Image" />
+                            <div class="px-6 py-4">
+                                <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">
+                                    {{ $menu->name }}</h4>
+                                <p class="leading-normal text-gray-700">{{ $menu->description }}.</p>
+                            </div>
+                            <div class="flex items-center justify-between p-4">
+                                <span class="text-xl text-green-600">${{ $menu->price }}</span>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </section>
     @endif
-    <section class="pt-4 pb-12 bg-gray-800">
-        <div class="my-16 text-center">
-            <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
-                Testimonial </h2>
-            <p class="text-xl text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. soluta sapient</p>
-        </div>
-        <div class="grid gap-2 lg:grid-cols-3">
-            <div class="max-w-md p-4 bg-white rounded-lg shadow-lg">
-                <div class="flex justify-center -mt-16 md:justify-end">
-                    <img class="object-cover w-20 h-20 border-2 border-green-500 rounded-full"
-                        src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80">
-                </div>
-                <div>
-                    <h2 class="text-3xl font-semibold text-gray-800">Food</h2>
-                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores
-                        deserunt
-                        ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis
-                        pariatur
-                        minus consequuntur!</p>
-                </div>
-                <div class="flex justify-end mt-4">
-                    <a href="#" class="text-xl font-medium text-green-500">John Doe</a>
-                </div>
-            </div>
-            <div class="max-w-md p-4 bg-white rounded-lg shadow-lg">
-                <div class="flex justify-center -mt-16 md:justify-end">
-                    <img class="object-cover w-20 h-20 border-2 border-green-500 rounded-full"
-                        src="https://cdn.pixabay.com/photo/2018/01/04/21/15/young-3061652__340.jpg">
-                </div>
-                <div>
-                    <h2 class="text-3xl font-semibold text-gray-800">Food</h2>
-                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores
-                        deserunt
-                        ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis
-                        pariatur
-                        minus consequuntur!</p>
-                </div>
-                <div class="flex justify-end mt-4">
-                    <a href="#" class="text-xl font-medium text-green-500">John Doe</a>
-                </div>
-            </div>
-            <div class="max-w-md p-4 bg-white rounded-lg shadow-lg">
-                <div class="flex justify-center -mt-16 md:justify-end">
-                    <img class="object-cover w-20 h-20 border-2 border-green-500 rounded-full"
-                        src="https://cdn.pixabay.com/photo/2018/01/18/17/48/purchase-3090818__340.jpg">
-                </div>
-                <div>
-                    <h2 class="text-3xl font-semibold text-gray-800">Food</h2>
-                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores
-                        deserunt
-                        ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis
-                        pariatur
-                        minus consequuntur!</p>
-                </div>
-                <div class="flex justify-end mt-4">
-                    <a href="#" class="text-xl font-medium text-green-500">John Doe</a>
-                </div>
-            </div>
-        </div>
-    </section>
 </x-guest-layout>
